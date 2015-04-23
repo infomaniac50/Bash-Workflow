efcadd14c18e1bcb65b858865c7b5a3584d8e079
Derek Chafin <infomaniac50@gmail.com>
2015-04-23 14:52:18 -0500

    Only iterate through .sh files.

--- test.sh
+++ test.sh
@@ -25,7 +25,7 @@ fi
 
 mkdir -p "${log_dir}/tests"
 
-for test in tests/*; do
+for test in tests/*.sh; do
   output_log="${log_dir}/${test}_output.log" 
   error_log="${log_dir}/${test}_error.log"
   files_pass="${log_dir}/${test}_pass.log" 
