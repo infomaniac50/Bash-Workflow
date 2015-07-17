3d06cfd0fa3f74daf889288e30dca2ec57b92c62
Derek Chafin <infomaniac50@gmail.com>
2015-07-17 10:53:10 -0500

    Add a note reminding myself of the pain I went through trying to get this to work.

--- src/system/init_unicode.sh
+++ src/system/init_unicode.sh
@@ -2,5 +2,6 @@
 
 if [[ $TERM == "linux" ]]; then
   # The linux console does not support unicode. Period!
+  # Believe me I've tried. It's got too many hoops to jump through.
   UNICODE_WORKFLOW="FALSE"
 fi
