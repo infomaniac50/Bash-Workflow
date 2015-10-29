2ffac6f78c000e86d97835b077ab4b7031cf9607
Derek Chafin <infomaniac50@gmail.com>
2015-10-29 16:02:49 -0500

    Refactor some common functions into a separate library.

---
+++ src/system/export.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Let the new shell use our PATH
+export PATH
+
+# Bash Workflow is now setup.
+export BASHWF
+export BASHWF_SYS
+export BASHWF_USER
+
+# Export some commands too
+export command_exists
---
+++ src/system/sys_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source "${BASHWF_SYS}/config.sh"
+source "${BASHWF_SYS}/utils.sh"
+source "${BASHWF_SYS}/init_unicode.sh"
+source "${BASHWF_SYS}/init_colors.sh"
